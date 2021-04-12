5d5f961020d3dc3b6f425e2f2364078e2d78c38d
Eldar <hasanovicel@gmail.com>
2021-04-12 13:31:33 +1000

    fixing number detection

--- src/Components/OrbitList.tsx
+++ src/Components/OrbitList.tsx
@@ -1,61 +1,8 @@
 
 import { Orbit } from '../Orbits/Orbit'
-import { Card } from 'antd';
-import { Input } from 'antd';
-import { useState } from 'react';
+import { Card} from 'antd';
 
-const AddOrbitModal = (props: {
-  addOrbitFn: (orbit: Orbit) => void
-}) => {
-
-  const { addOrbitFn } = props
-
-  const [ e, setE ] = useState(0)
-  const [ a, setA ] = useState(0)
-  const [ i, setI ] = useState(0)
-  const [ long, setLong ] = useState(0)
-  const [ arg, setArg ] = useState(0)
-  const [ t, setT ] = useState(0)
-
-  const filterInput = (input: string): number => {
-    return parseInt(input.replace(/\D/g,'')) || 0
-  }
-
-  const resetInput = () => {
-    setE(0)
-    setA(0)
-    setI(0)
-    setLong(0)
-    setArg(0)
-    setT(0)
-  }
-
-  return (
-    <Card
-      size="small"
-      title="Orbit"
-      style={{ width: 300 }}
-      extra={<a onClick={() => { 
-        addOrbitFn({
-          eccentricity: e,
-          semimajorAxis: a,
-          inclination: i,
-          longitudeOfAscendingNode: long,
-          argumentOfPeriapsis: arg,
-          trueAnomaly: t,
-        })
-        resetInput()
-      }}>Create</a>}
-    >
-      <Input addonBefore='Eccentricity' value={e} onChange={e => setE(filterInput(e.target.value))}/>
-      <Input addonBefore='Semimajor Axis' value={a} onChange={e => setA(filterInput(e.target.value))}/>
-      <Input addonBefore='Inclination' value={i} onChange={e => setI(filterInput(e.target.value))}/>
-      <Input addonBefore='Long. of Acend.' value={long} onChange={e => setLong(filterInput(e.target.value))}/>
-      <Input addonBefore='Arg. of Peri.' value={arg} onChange={e => setArg(filterInput(e.target.value))}/>
-      <Input addonBefore='True Anomaly' value={t} onChange={e => setT(filterInput(e.target.value))}/>
-    </Card>
-  )
-}
+import AddOrbitModal from './AddOrbitModal'
 
 const OrbitListItem = (props: {
   id: string
@@ -70,7 +17,7 @@ const OrbitListItem = (props: {
       size="small"
       title="Orbit"
       style={{ width: 300 }}
-      extra={<a onClick={() => { rmOrbitFn(id) }}>Delete</a>}
+      extra={<a onClick={() => { rmOrbitFn(id) }} style={{ color: "red" }}>Delete</a>}
     >
       <p> Eccentricity {orbit.eccentricity} </p>
       <p> Semimajor Axis {orbit.semimajorAxis} </p>
@@ -95,6 +42,7 @@ const OrbitList = (props: {
       {
         orbits.map(o =>
           <OrbitListItem
+            key={o.id}
             id={o.id}
             orbit={o.orbit}
             rmOrbitFn={rmOrbitFn}
